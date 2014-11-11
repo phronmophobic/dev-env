5a1c191a0c7d3d23a2a2c84e00b59b2b0d8b35bd
Adrian Smith <smith.adriane@gmail.com>
2014-11-11 11:30:22 -0800

    dont use auto merge for ido.

--- .emacs.d/init-ido.el
+++ .emacs.d/init-ido.el
@@ -7,7 +7,7 @@
 (ido-ubiquitous t)
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point nil)
-(setq ido-auto-merge-work-directories-length 0)
+(setq ido-auto-merge-work-directories-length -1)
 (setq ido-use-virtual-buffers t)
 
 ;; Allow the same buffer to be open in different frames
